52dda43047face681526f9616e685b9c222f023c
Darshan-Paliwal <darshanpaliwal.work@gmail.com>
2025-08-23 16:25:11 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,24 +15,6 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <main>{children}</main>
-        <footer
-          style={{
-            textAlign: "center",
-            marginTop: "20px",
-            padding: "10px",
-            fontSize: "14px",
-            color: "#666",
-          }}
-        >
-          Created by Darshan Paliwal
-          <br />
-          <a
-            href="https://darshanpaliwal.netlify.app"
-            style={{ color: "#0070f3", textDecoration: "none" }}
-          >
-            darshanpaliwal.netlify.app
-          </a>
-        </footer>
       </body>
     </html>
   );
